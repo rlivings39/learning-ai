4b38aae9c8b6db5caa9444f223d81543ac3a4054
Ryan Livingston <43882944+rlivings39@users.noreply.github.com>
2025-02-24 16:48:19 -0500

    Practical Python: Exercise 1.5 bouncing ball

--- python-lessons/practical-python/Work/bounce.py
+++ python-lessons/practical-python/Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+def main():
+    height = 100 # meters
+    for bounce_number in range(10):
+        height = round(height*(3/5), 4)
+        print(bounce_number+1, height)
+
+if __name__ == "__main__":
+    main()
