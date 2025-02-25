84a141474dfa4f49651e7c081a6332affaafa6c1
Ryan Livingston <43882944+rlivings39@users.noreply.github.com>
2025-02-24 20:38:45 -0500

    Practical Python: File reading tweaks

--- python-lessons/practical-python/Work/pcost.py
+++ python-lessons/practical-python/Work/pcost.py
@@ -1,11 +1,11 @@
 # pcost.py
 #
 # Exercise 1.27
-import os, csv
-def main():
+import os, csv, sys
+def main(filename):
     this_folder = os.path.dirname(os.path.realpath(__file__))
     total_price = 0.0
-    with open(os.path.join(this_folder, 'Data', 'portfolio.csv'), 'rt', encoding='utf-8') as file:
+    with open(os.path.join(this_folder, filename), 'rt', encoding='utf-8') as file:
         csvfile = csv.reader(file)
         # Skip header
         next(csvfile)
@@ -18,4 +18,4 @@ def main():
     print(f'Total cost {total_price:0.2f}')
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1] if len(sys.argv) == 2 else os.path.join('Data', 'portfolio.csv'))
