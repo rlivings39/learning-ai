bd7cc9a91898f9a3d128e990a0e376c12151ae9e
Ryan Livingston <43882944+rlivings39@users.noreply.github.com>
2025-02-24 20:08:03 -0500

    Practical Python: Use CSV module

--- python-lessons/practical-python/Work/pcost.py
+++ python-lessons/practical-python/Work/pcost.py
@@ -1,18 +1,18 @@
 # pcost.py
 #
 # Exercise 1.27
-import os
+import os, csv
 def main():
     this_folder = os.path.dirname(os.path.realpath(__file__))
     total_price = 0.0
-    with open(os.path.join(this_folder, 'Data', 'portfolio.csv'), 'rt', encoding='utf-8') as csvfile:
+    with open(os.path.join(this_folder, 'Data', 'portfolio.csv'), 'rt', encoding='utf-8') as file:
+        csvfile = csv.reader(file)
         # Skip header
         next(csvfile)
         for line in csvfile:
-            data = line.split(',')
-            name = data[0].strip()
-            shares = int(data[1])
-            price = float(data[2])
+            name = line[0].strip()
+            shares = int(line[1])
+            price = float(line[2])
             total_price += shares*price
 
     print(f'Total cost {total_price:0.2f}')
