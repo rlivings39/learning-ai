344ec9a70f4ade7481839e534dcd8ee0d7d714be
Ryan Livingston <43882944+rlivings39@users.noreply.github.com>
2025-02-27 05:51:15 -0500

    Practical Python: Functions and reworking CSV parsing example

--- python-lessons/practical-python/Work/fileparse.py
+++ python-lessons/practical-python/Work/fileparse.py
@@ -4,25 +4,24 @@
 import csv
 
 # TODO overly complicated and doesn't respect order of select list in output/types
-def parse_csv(filename: str, select: list = [], types: list = []):
+def parse_csv(filename: str, select: list = None, types: list = None):
     '''
     Parse a CSV file into a list of records passing an optional column filter
     '''
-    with open(filename) as f:
+    with open(filename, encoding='utf-8') as f:
         rows = csv.reader(f)
 
         # Read headers
         headers = next(rows)
+        indices = []
         if select:
-            indices_headers = [(i,h) for i,h in enumerate(headers) if h in select]
-            headers = [h for _,h in indices_headers]
-        else:
-            indices_headers = list(zip(range(len(headers)), headers))
+            indices = [headers.index(s) for s in select]
+            headers = select
         records = []
         for row in rows:
             if not row: # skip empty rows
                 continue
-            row = [row[i] for i,_ in indices_headers]
+            row = [row[i] for i in indices]
             if types:
                 row = [f(val) for f,val in zip(types, row)]
             record = dict(zip(headers, row))
