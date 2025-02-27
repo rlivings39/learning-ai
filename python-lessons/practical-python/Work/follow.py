1dbd404805c45018c4ae961b52c6c3ebce063f2b
Ryan Livingston <43882944+rlivings39@users.noreply.github.com>
2025-02-27 15:03:59 -0500

    Practical Python: Generators are cool

---
+++ python-lessons/practical-python/Work/follow.py
@@ -0,0 +1,33 @@
+'''
+Use generators to monitor stock simulator output
+'''
+import os
+import time
+import report
+
+def follow(filename):
+    '''
+    Watches the file specified by filename for output and yields new lines when found
+    '''
+    with open(filename,'rt',encoding="utf-8") as f:
+        f.seek(0, os.SEEK_END)
+        while True:
+            line = f.readline()
+            if line == '':
+                time.sleep(0.1) # wait for more
+                continue
+            yield line
+
+def main():
+    'follow.py main'
+    pf = report.read_portfolio('Data/portfolio.csv')
+    for line in follow('Data/stocklog.csv'):
+        fields = line.split(',')
+        name = fields[0].strip('"')
+        price = float(fields[1])
+        change = float(fields[4])
+        if name in pf:
+            print(f'{name:>10s} {price:>10.2f} {change:>10.2f}')
+
+if __name__ == "__main__":
+    main()
