024b58de46dbf60be9a1e4a6a021e6ad045149a9
Ryan Livingston <43882944+rlivings39@users.noreply.github.com>
2025-02-26 19:52:48 -0500

    Practical Python: Sequences and iteration

--- python-lessons/practical-python/Work/pcost.py
+++ python-lessons/practical-python/Work/pcost.py
@@ -3,19 +3,23 @@
 # Exercise 1.27
 import os, csv, sys
 def main(filename):
-    this_folder = os.path.dirname(os.path.realpath(__file__))
     total_price = 0.0
     with open(os.path.join(this_folder, filename), 'rt', encoding='utf-8') as file:
         csvfile = csv.reader(file)
-        # Skip header
-        next(csvfile)
-        for line in csvfile:
-            name = line[0].strip()
-            shares = int(line[1])
-            price = float(line[2])
-            total_price += shares*price
+        header = next(csvfile)
+        for linenum, line in enumerate(csvfile, start=1):
+            record = dict(zip(header, line))
+            try:
+                name = record['name'].strip()
+                shares = int(record['shares'])
+                price = float(record['price'])
+                total_price += shares*price
+            except ValueError:
+                print(f'Row {linenum}: Couldn\'t convert {line}')
 
     print(f'Total cost {total_price:0.2f}')
+
+this_folder = os.path.dirname(os.path.realpath(__file__))
 
 if __name__ == "__main__":
     main(sys.argv[1] if len(sys.argv) == 2 else os.path.join('Data', 'portfolio.csv'))
