851c1ea22c95cfbe10f63c1ef53be5443a44fafc
Ryan Livingston <43882944+rlivings39@users.noreply.github.com>
2025-03-02 18:56:35 -0500

    Practical Python: First test

---
+++ python-lessons/practical-python/Work/test_stock.py
@@ -0,0 +1,17 @@
+'''
+Unit tests for stock.py
+'''
+
+import unittest
+import stock
+
+class TestStock(unittest.TestCase):
+    def test_create(self):
+        s = stock.Stock('GOOG',100,490.1)
+        self.assertEqual(s.name, 'GOOG')
+        self.assertEqual(s.shares, 100)
+        self.assertEqual(s.price, 490)
+        self.assertEqual(s.cost, 100*490.1)
+
+if __name__ == "__main__":
+    unittest.main()
