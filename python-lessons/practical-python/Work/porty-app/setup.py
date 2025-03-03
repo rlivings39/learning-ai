15de653a3876f4b96f632c41d980552bae3ce2cf
Ryan Livingston <43882944+rlivings39@users.noreply.github.com>
2025-03-03 08:33:45 -0500

    Practical Python: Make porty-app packageable and distributable

---
+++ python-lessons/practical-python/Work/porty-app/setup.py
@@ -0,0 +1,12 @@
+'''
+Setup for porty package from Practical Python
+'''
+import setuptools
+
+setuptools.setup(
+    name='porty',
+    version='0.0.1',
+    author='rlivings39',
+    description='Practical Python Example Code',
+    packages=setuptools.find_packages()
+)
