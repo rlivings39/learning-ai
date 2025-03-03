e7d4941562c5eef29336f12bddf93c83985282ea
Ryan Livingston <43882944+rlivings39@users.noreply.github.com>
2025-03-02 19:21:26 -0500

    Practical Python: Finish testing, fix pylint import checking, get test viewer working

--- python-lessons/practical-python/Work/test_stock.py
+++ python-lessons/practical-python/Work/test_stock.py
@@ -13,5 +13,22 @@ class TestStock(unittest.TestCase):
         self.assertEqual(s.price, 490.1)
         self.assertEqual(s.cost, 100*490.1)
 
+    def test_cost(self):
+        s = stock.Stock('GOOG',100,490.1)
+        self.assertEqual(s.cost, 100*490.1)
+
+    def test_sell(self):
+        s = stock.Stock('GOOG',100,490.1)
+        self.assertEqual(s.shares, 100)
+        s.sell(6)
+        self.assertEqual(s.shares, 94)
+        s.sell(0)
+        self.assertEqual(s.shares, 94)
+
+    def test_shares_type(self):
+        s = stock.Stock('GOOG',100,490.1)
+        with self.assertRaises(TypeError):
+            s.shares = 'a'
+
 if __name__ == "__main__":
     unittest.main()
