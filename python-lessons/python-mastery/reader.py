c441d8a2985c96c9749ec4615669e8b8ad0ad837
Ryan Livingston <43882944+rlivings39@users.noreply.github.com>
2025-03-18 12:26:12 -0400

    Advanced Python mastery: Format code

--- python-lessons/python-mastery/reader.py
+++ python-lessons/python-mastery/reader.py
@@ -5,42 +5,47 @@ import logging
 
 log = logging.getLogger(__name__)
 
+
 def convert_csv(lines, converter, *, headers=None):
     rows = csv.reader(lines)
     if headers is None:
         headers = next(rows)
     res = []
-    for rownum,row in enumerate(rows):
+    for rownum, row in enumerate(rows):
         try:
             res.append(converter(headers, row))
         except ValueError as e:
-            log.warning(f'Row {rownum}: Bad row {row}')
-            log.debug(f'Row {rownum}: Reason: {e}')
+            log.warning(f"Row {rownum}: Bad row {row}")
+            log.debug(f"Row {rownum}: Reason: {e}")
 
     return res
 
+
 def csv_as_dicts(lines, types, *, headers=None):
-    return convert_csv(lines,
-                       lambda headers, row: {name: func(val) for name, func, val in zip(headers, types, row)},
-                       headers=headers)
+    return convert_csv(
+        lines,
+        lambda headers, row: {
+            name: func(val) for name, func, val in zip(headers, types, row)
+        },
+        headers=headers,
+    )
 
 
 def csv_as_instances(lines, cls, *, headers=None):
-    return convert_csv(lines,
-                       lambda headers, row: cls.from_row(row),
-                       headers=headers)
+    return convert_csv(lines, lambda headers, row: cls.from_row(row), headers=headers)
+
 
 def read_csv_as_dicts(filename, types, *, headers=None):
-    '''
+    """
     Read CSV data into a list of dictionaries with optional type conversion
-    '''
+    """
     with open(filename) as file:
         return csv_as_dicts(file, types, headers=headers)
 
+
 def read_csv_as_instances(filename, cls, *, headers=None):
-    '''
+    """
     Read CSV data into a list of instances
-    '''
+    """
     with open(filename) as file:
         return csv_as_instances(file, cls, headers=headers)
-
--- python-lessons/python-mastery/validate.py
+++ python-lessons/python-mastery/validate.py
@@ -1,6 +1,7 @@
-'''
+"""
 Validation and type checking
-'''
+"""
+
 
 class Validator:
     def __init__(self, name=None):
@@ -17,50 +18,62 @@ class Validator:
 
     def __set_name__(self, cls, name):
         self.name = name
+
+
 class Typed(Validator):
     expected_type = object
+
     @classmethod
     def check(cls, value):
         if not isinstance(value, cls.expected_type):
-            raise TypeError(f'Expected {cls.expected_type}; got a {type(value)}')
+            raise TypeError(f"Expected {cls.expected_type}; got a {type(value)}")
         return super().check(value)
+
 
 class Integer(Typed):
     expected_type = int
 
+
 class Float(Typed):
     expected_type = float
 
+
 class String(Typed):
     expected_type = str
+
 
 class Positive(Validator):
     @classmethod
     def check(cls, value):
         if value < 0:
-            raise ValueError(f'Expected positive value. Received {value!r}')
+            raise ValueError(f"Expected positive value. Received {value!r}")
         return super().check(value)
+
 
 class NonEmpty(Validator):
     @classmethod
     def check(cls, value):
         if len(value) == 0:
-            raise ValueError(f'Expected non-empty value. Received {value!r}')
+            raise ValueError(f"Expected non-empty value. Received {value!r}")
         return super().check(value)
+
 
 # Now compose multiple of these validators to form new ones. These invoke the
 # parent check() methods in the order listed
 class PositiveInteger(Integer, Positive):
     pass
 
+
 class PositiveFloat(Float, Positive):
     pass
+
 
 class NonEmptyString(String, NonEmpty):
     pass
 
+
 class UsesDescriptor:
-    a = PositiveInteger('a')
+    a = PositiveInteger("a")
 
     def __init__(self, val):
         self.a = val
