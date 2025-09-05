b9a53458837ae7926f6d4a6a66963968d0b26881
Ryan Livingston <43882944+rlivings39@users.noreply.github.com>
2025-09-05 11:36:26 -0400

    Add an RNN and basic tests

--- pytorch-intro/char_classification/char_classification.py
+++ pytorch-intro/char_classification/char_classification.py
@@ -10,8 +10,11 @@ import string
 import time
 import unicodedata
 from io import open
+from typing import List
 
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 from torch.utils.data import Dataset
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
@@ -49,19 +52,81 @@ def line_to_tensor(line: str):
 
 
 class NamesDataset(Dataset):
-    def __init__(self, data_dir: str):
+    def __init__(self, data_dir: str | None = None):
+        if data_dir is None:
+            data_dir = os.path.join(
+                os.path.dirname(os.path.abspath(__file__)), "data", "names"
+            )
+
         # Provenance of data
         self.data_dir = data_dir
         self.load_time = time.localtime
         labels_set = set()
-        self.data = []
-        self.data_tensors = []
-        self.labels = []
-        self.labels_tensors = []
+        self.data: List[str] = []
+        self.data_tensors: List[torch.Tensor] = []
+        self.labels: List[str] = []
+        self.labels_tensors: List[torch.Tensor] = []
 
         # Read .txt files in given folder
         text_files = glob.glob(os.path.join(data_dir, "*.txt"))
         for filename in text_files:
             label, _ = os.path.splitext(os.path.basename(filename))
+            labels_set.add(label)
+            with open(filename, encoding="utf-8") as f:
+                lines = f.read().strip().split("\n")
+                for name in lines:
+                    self.data.append(name)
+                    self.data_tensors.append(line_to_tensor(name))
+                    self.labels.append(label)
+        # Cache tensors for labels
+        self.labels_unique = list(labels_set)
+        for label in self.labels:
+            temp_tensor = torch.tensor(
+                [self.labels_unique.index(label)], dtype=torch.long
+            )
+            self.labels_tensors.append(temp_tensor)
 
-        pass
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx: int):
+        data_item = self.data[idx]
+        data_label = self.labels[idx]
+        data_tensor = self.data_tensors[idx]
+        label_tensor = self.labels_tensors[idx]
+        return label_tensor, data_tensor, data_label, data_item
+
+    def label_from_output(self, output):
+        top_n, top_i = output.topk(1)
+        label_i = top_i[0].item()
+        return self.labels_unique[label_i], label_i
+
+
+class CharRNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(CharRNN, self).__init__()
+        self.rnn = nn.RNN(input_size, hidden_size)
+        self.hidden_to_output = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, line_tensor: torch.Tensor):
+        rnn_out, hidden = self.rnn(line_tensor)
+        output = self.hidden_to_output(hidden[0])
+        output = self.softmax(output)
+        return output
+
+
+def main():
+    ds = NamesDataset()
+
+    # Get our training a test data
+    train_set, test_set = torch.utils.data.random_split(
+        ds, [0.85, 0.15], generator=torch.Generator(device=device).manual_seed(2024)
+    )
+    n_hidden = 128
+    rnn = CharRNN(n_letters, n_hidden, len(ds.labels_unique))
+    print(rnn)
+
+
+if __name__ == "__main__":
+    main()
--- pytorch-intro/char_classification/test_char_classification.py
+++ pytorch-intro/char_classification/test_char_classification.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 
 from . import char_classification
@@ -6,3 +8,22 @@ from . import char_classification
 def test_letter_to_index():
     assert char_classification.letter_to_index("a") == 0
     assert char_classification.letter_to_index("g") == 6
+
+
+def test_dataset():
+    ds = char_classification.NamesDataset()
+    assert len(ds.labels) > 0
+    first = ds[0]
+    assert len(first) == 4
+    assert (len(ds)) == 20074
+
+
+def test_basic_forward():
+    ds = char_classification.NamesDataset()
+    rnn = char_classification.CharRNN(
+        char_classification.n_letters, 128, len(ds.labels_unique)
+    )
+    out = rnn(char_classification.line_to_tensor("Albert"))
+    label, label_idx = ds.label_from_output(out)
+    assert label in ds.labels_unique
+    assert label == ds.labels_unique[label_idx]
