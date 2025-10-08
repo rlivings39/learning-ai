1bef54a2a29dbb0c62b40b43e4bd3d762a0a0ad6
Ryan Livingston <43882944+rlivings39@users.noreply.github.com>
2025-10-08 11:58:29 -0400

    Working linear regression

--- ai-courses-and-books/dive-into-deep-learning/linear_regression.py
+++ ai-courses-and-books/dive-into-deep-learning/linear_regression.py
@@ -3,6 +3,7 @@
 import random
 import time
 from dataclasses import dataclass
+from typing import List
 
 import numpy as np
 import torch
@@ -11,12 +12,32 @@ from d2l import torch as d2l
 from torch import nn
 
 
-class Module(nn.Module, d2l.HyperParameters):
+@dataclass
+class SGD:
+    """Minibatch stochastic gradient descent"""
+
+    params: List[torch.Tensor]
+    learning_rate: float
+
+    def step(self):
+        for param in self.params:
+            param -= self.learning_rate * param.grad
+
+    def zero_grad(self):
+        for param in self.params:
+            if param.grad is not None:
+                param.grad.zero_()
+
+
+@dataclass
+class Module(nn.Module):
     """Base class of our models"""
 
-    def __init__(self, plot_train_per_epoch=2, plot_valid_per_epoch=1):
+    plot_train_per_epoch = 2
+    plot_valid_per_epoch = 1
+
+    def __post_init__(self):
         super().__init__()
-        self.save_hyperparameters()
         self.board = d2l.ProgressBoard()
 
     def loss(self, y_hat, y):
@@ -43,23 +64,25 @@ class Module(nn.Module, d2l.HyperParameters):
             every_n=int(n),
         )
 
-    def training_step(self, batch):
+    def training_step(self, batch) -> torch.Tensor:
         l = self.loss(self(*batch[:-1]), batch[-1])
         self.plot("loss", l, train=True)
+        return l
 
     def validation_step(self, batch):
         l = self.loss(self(*batch[:-1]), batch[-1])
         self.plot("loss", l, train=False)
 
-    def configure_optimizers(self):
+    def configure_optimizers(self) -> SGD:
         raise NotImplementedError
 
 
-class DataModule(d2l.HyperParameters):
+@dataclass
+class DataModule:
     """Base class of data"""
 
-    def __init__(self, root="../data", num_workers=4):
-        self.save_hyperparameters()
+    root = "../data"
+    num_workers = 4
 
     def get_dataloader(self, train):
         raise NotImplementedError
@@ -90,6 +113,7 @@ class Trainer:
     def prepare_model(self, model: Module):
         model.trainer = self
         model.board.xlim = [0, self.max_epochs]
+        self.model = model
 
     def fit(self, model: Module, data: DataModule):
         self.prepare_data(data)
@@ -103,6 +127,35 @@ class Trainer:
 
     def fit_epoch(self):
         raise NotImplementedError
+
+    def prepare_batch(self, batch):
+        return batch
+
+    def clip_gradients(self, grad_clip_val, model):
+        params = [p for p in model.parameters() if p.requires_grad]
+        norm = torch.sqrt(sum(torch.sum((p.grad**2)) for p in params))
+        if norm > grad_clip_val:
+            for param in params:
+                param.grad[:] *= grad_clip_val / norm
+
+    def fit_epoch(self):
+        self.model.train()
+        for batch in self.train_dataloader:
+            loss = self.model.training_step(self.prepare_batch(batch))
+            self.optim.zero_grad()
+            with torch.no_grad():
+                loss.backward()
+                if self.gradient_clip_val > 0:
+                    self.clip_gradients(self.gradient_clip_val, self.model)
+                self.optim.step()
+            self.train_batch_idx += 1
+        if self.val_dataloader is None:
+            return
+        self.model.eval()
+        for batch in self.val_dataloader:
+            with torch.no_grad():
+                self.model.validation_step(self.prepare_batch(batch))
+            self.val_batch_idx += 1
 
 
 @dataclass
@@ -130,3 +183,30 @@ class SyntheticRegressionData(DataModule):
     def get_dataloader(self, train: bool):
         i = slice(0, self.num_train) if train else slice(self.num_train, None)
         return self.get_tensorloader((self.X, self.y), train, i)
+
+
+@dataclass
+class LinearRegressionScratch(Module):
+    """A linear regression model implemented from scratch"""
+
+    num_inputs: int
+    learning_rate: float
+    sigma = 0.01
+
+    def __post_init__(self):
+        super().__post_init__()
+        # Initialize weights to normally distributed values
+        self.w = torch.normal(0, self.sigma, (self.num_inputs, 1), requires_grad=True)
+        # Initialize bias to 0
+        self.b = torch.zeros(1, requires_grad=True)
+
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
+        """Forward prop where X is a tensor whose rows are examples. For a single sample pass a row vector."""
+        return torch.matmul(X, self.w) + self.b
+
+    def loss(self, y_hat, y):
+        l = ((y_hat - y) ** 2) / 2
+        return l.mean()
+
+    def configure_optimizers(self):
+        return SGD([self.w, self.b], self.learning_rate)
